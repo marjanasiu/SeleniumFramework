a72f1668568528b9bf27fd3dcee00713aca4d497
Marjana A <marjanasiu@gmail.com>
2020-01-23 21:28:22 -0500

    first commit completed

--- Facebook/src/main/java/home/HomePage.java
+++ Facebook/src/main/java/home/HomePage.java
@@ -6,9 +6,9 @@ import org.openqa.selenium.By;
 public class HomePage extends CommonAPI {
 
     public void login(String email, String password) {
-        this.driver.findElement(By.id("email")).sendKeys(email);
-        this.driver.findElement(By.id("pass")).sendKeys(password);
-        this.driver.findElement(By.cssSelector("input[value=Log In]")).click();
+        driver.findElement(By.id("email")).sendKeys(email);
+        driver.findElement(By.id("pass")).sendKeys(password);
+        driver.findElement(By.cssSelector("input[value='Log In']")).click();
     }
 
 }
